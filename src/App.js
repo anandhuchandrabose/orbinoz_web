ee36bb42db5ea0920b39ce3b1382d6f1742e1311
manu-git-hub <ms.manu.856@gmail.com>
2024-07-31 04:11:33 +0530

    added route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 // src/App.js
 
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import WhatAreWe from './components/WhatAreWe';
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
+    // <div className="App">
+    //   <LandingPage />
      
-    </div>
+    // </div>
+    <Router>
+      <Routes>
+          <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
