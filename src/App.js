18d9585792e95fea899831efee9a6c241ec1c361
manu-git-hub <ms.manu.856@gmail.com>
2024-07-31 04:35:59 +0530

    changed back

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import WhatAreWe from './components/WhatAreWe';
 
 function App() {
   return (
-      // <div className="App">
-    //   <LandingPage />
-    // </div>
-
- //Manu Changed for production --cheers
-
-    <div className="App">
-    <Router>
-      <Routes>
-          <Route path="/" element={<LandingPage />} />
-      </Routes>
-    </Router>
+      <div className="App">
+      <LandingPage />
     </div>
   );
 }
